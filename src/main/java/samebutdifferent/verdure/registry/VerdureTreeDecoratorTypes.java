13064fb5df3ff0c7e7500efd7cfc789ee50782de
SameButDifferent <76499872+samedifferent@users.noreply.github.com>
2022-04-21 13:34:54 -0400

    Add branch Tree Decorator

--- src/main/java/samebutdifferent/verdure/registry/VerdureTreeDecoratorTypes.java
+++ src/main/java/samebutdifferent/verdure/registry/VerdureTreeDecoratorTypes.java
@@ -5,6 +5,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import samebutdifferent.verdure.Verdure;
+import samebutdifferent.verdure.worldgen.treedecorator.BranchDecorator;
 import samebutdifferent.verdure.worldgen.treedecorator.DaisiesDecorator;
 import samebutdifferent.verdure.worldgen.treedecorator.HollowLogDecorator;
 
@@ -13,5 +14,6 @@ public class VerdureTreeDecoratorTypes {
 
     public static final RegistryObject<TreeDecoratorType<DaisiesDecorator>> DAISIES = TREE_DECORATOR_TYPES.register("daisies", () -> new TreeDecoratorType<>(DaisiesDecorator.CODEC));
     public static final RegistryObject<TreeDecoratorType<HollowLogDecorator>> HOLLOW_LOG = TREE_DECORATOR_TYPES.register("hollow_log", () -> new TreeDecoratorType<>(HollowLogDecorator.CODEC));
+    public static final RegistryObject<TreeDecoratorType<BranchDecorator>> BRANCH = TREE_DECORATOR_TYPES.register("branch", () -> new TreeDecoratorType<>(BranchDecorator.CODEC));
 
 }
---
+++ src/main/java/samebutdifferent/verdure/worldgen/treedecorator/BranchDecorator.java
@@ -0,0 +1,56 @@
+package samebutdifferent.verdure.worldgen.treedecorator;
+
+import com.mojang.serialization.Codec;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.LevelSimulatedReader;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.treedecorators.TreeDecorator;
+import net.minecraft.world.level.levelgen.feature.treedecorators.TreeDecoratorType;
+import samebutdifferent.verdure.registry.VerdureTreeDecoratorTypes;
+
+import java.util.List;
+import java.util.Random;
+import java.util.function.BiConsumer;
+
+public class BranchDecorator extends TreeDecorator {
+    public static final Codec<BranchDecorator> CODEC = BlockStateProvider.CODEC.fieldOf("branch").xmap(BranchDecorator::new, (decorator) -> decorator.provider).codec();
+    private final BlockStateProvider provider;
+
+    public BranchDecorator(BlockStateProvider provider) {
+        this.provider = provider;
+    }
+
+    @Override
+    protected TreeDecoratorType<?> type() {
+        return VerdureTreeDecoratorTypes.BRANCH.get();
+    }
+
+    @Override
+    public void place(LevelSimulatedReader pLevel, BiConsumer<BlockPos, BlockState> pBlockSetter, Random pRandom, List<BlockPos> pLogPositions, List<BlockPos> pLeafPositions) {
+        if (!pLeafPositions.isEmpty()) {
+            int lowestLeafY = pLeafPositions.get(0).getY();
+            List<BlockPos> exposedLogs = pLogPositions.stream().filter(blockPos -> blockPos.getY() < lowestLeafY).toList();
+            Direction zRand = pRandom.nextBoolean() ? Direction.NORTH : Direction.SOUTH;
+            Direction xRand = pRandom.nextBoolean() ? Direction.EAST : Direction.WEST;
+            BlockPos posFirst = exposedLogs.get(exposedLogs.size() - 1);
+            BlockPos posSecond = exposedLogs.get(exposedLogs.size() - 2);
+            if (exposedLogs.size() < 5) {
+                Direction direction = pRandom.nextBoolean() ? xRand : zRand;
+                if (Feature.isAir(pLevel, posFirst.relative(direction))) {
+                    pBlockSetter.accept(posFirst.relative(direction), this.provider.getState(pRandom, posFirst).setValue(HorizontalDirectionalBlock.FACING, direction));
+                }
+            } else {
+                if (Feature.isAir(pLevel, posFirst.relative(xRand))) {
+                    pBlockSetter.accept(posFirst.relative(xRand), this.provider.getState(pRandom, posFirst).setValue(HorizontalDirectionalBlock.FACING, xRand));
+                }
+                if (Feature.isAir(pLevel, posSecond.relative(zRand))) {
+                    pBlockSetter.accept(posSecond.relative(zRand), this.provider.getState(pRandom, posSecond).setValue(HorizontalDirectionalBlock.FACING, zRand));
+                }
+            }
+        }
+    }
+}
